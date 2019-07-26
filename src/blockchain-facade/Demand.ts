336b2f96e5cb863599eee7eff9c93b6bb3fc6b2a
Joseph Bagaric <joseph@josephit.com>
2019-07-26 13:28:32 +0200

    Cleanup

--- src/blockchain-facade/Demand.ts
+++ src/blockchain-facade/Demand.ts
@@ -73,9 +73,6 @@ export const createDemand = async (
             privateKey: configuration.blockchainProperties.activeUser.privateKey
         }
     );
-    console.log({
-        tx
-    });
 
     demand.id = configuration.blockchainProperties.web3.utils
         .hexToNumber(tx.logs[0].topics[1])
