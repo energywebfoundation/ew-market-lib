153a36d753b1f3670b51b00e7269e64dd4228e22
Daniel Kmak <neverkuzi@gmail.com>
2019-07-26 02:07:50 +0200

    Add more offchain props

--- src/blockchain-facade/Supply.ts
+++ src/blockchain-facade/Supply.ts
@@ -22,6 +22,8 @@ export interface ISupplyOffchainProperties {
     currency: GeneralLib.Currency;
     availableWh: number;
     timeframe: GeneralLib.TimeFrame;
+    startTime: string;
+    endTime: string;
 }
 
 export interface ISupplyOnChainProperties
